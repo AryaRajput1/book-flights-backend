917d0165a0f91e73882d667c2ef0346daee6b85b
Arya Rajput <rajputarya10@gmail.com>
2025-04-28 19:08:29 +0530

    created city service

---
+++ flightSearch/src/services/cityService.js
@@ -0,0 +1,45 @@
+const { CityRepository } = require("../repository")
+
+class CityService {
+
+    static async createCity(name) {
+        try {
+            return await CityRepository.createCity(name)
+        } catch (error) {
+            console.log('Error creating city.')
+            throw error
+        }
+    }
+
+    static async getCity(id) {
+        try {
+            return await CityRepository.getCity(id)
+        } catch (error) {
+            console.log('Error getting city.')
+            throw error
+        }
+    }
+
+    static async updateCity(id, name) {
+        try {
+            return await CityRepository.updateCity(id, name)
+        } catch (error) {
+            console.log('Error updating city.')
+            throw error
+        }
+    }
+
+    static async deleteCity(id) {
+        try {
+            return await CityRepository.deleteCity(id)
+
+        } catch (error) {
+            console.log('Error creating city.')
+            throw error
+        }
+    }
+}
+
+module.exports = {
+    CityService
+}
